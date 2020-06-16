560528d9df87359db590898c5d2cef01c9d49711
Cường Trần <46061394+cuonganh@users.noreply.github.com>
2020-06-17 01:30:25 +0700

    Thêm bài đọc

--- source/models/dataSchema.js
+++ source/models/dataSchema.js
@@ -15,7 +15,7 @@ const userSchema = new Schema({
     test: { type: Boolean, default: true }
 })
 
-//ma hoa password theo chuan bcrypt
+//ma hoa password theo chaun bcrypt
 userSchema.pre("save", function (next) {
     if (!this.isModified("userPassword")) {
         return next()
@@ -39,7 +39,6 @@ const userScoreSchema = new Schema({
         timestamps: { createdAt: "CreatedAt" }
     })
 
-
 const tracnghiemSchema = new Schema({
     tnQuestionContent: { type: String, required: true },
     tnAnswer: {
@@ -48,7 +47,9 @@ const tracnghiemSchema = new Schema({
         AnswerBContent: { type: String, required: true },
         AnswerCContent: { type: String, required: true },
         AnswerDContent: { type: String, required: true },
+
     }
+
 })
 
 const dientuSchema = new Schema({
@@ -59,18 +60,66 @@ const dientuSchema = new Schema({
         AnswerBContent: { type: String, required: true },
         AnswerCContent: { type: String, required: true },
         AnswerDContent: { type: String, required: true },
+
     }
 })
 
+const baidocSchema = new Schema({
+    bdParagraph: { type: String, required: true },
+    bdQuestion: {
+        Question1: {
+            Q1QuestionContent: { type: String, require: true },
+            Q1AnswerValue: { type: String, require: true },
+            Q1AnswerAContent: { type: String, required: true },
+            Q1AnswerBContent: { type: String, required: true },
+            Q1AnswerCContent: { type: String, required: true },
+            Q1AnswerDContent: { type: String, required: true },
+        },
+        Question2: {
+            Q2QuestionContent: { type: String, require: true },
+            Q2AnswerValue: { type: String, require: true },
+            Q2AnswerAContent: { type: String, required: true },
+            Q2AnswerBContent: { type: String, required: true },
+            Q2AnswerCContent: { type: String, required: true },
+            Q2AnswerDContent: { type: String, required: true },
+        },
+        Question3: {
+            Q3QuestionContent: { type: String, require: true },
+            Q3AnswerValue: { type: String, require: true },
+            Q3AnswerAContent: { type: String, required: true },
+            Q3AnswerBContent: { type: String, required: true },
+            Q3AnswerCContent: { type: String, required: true },
+            Q3AnswerDContent: { type: String, required: true },
+        },
+        Question4: {
+            Q4QuestionContent: { type: String, require: true },
+            Q4AnswerValue: { type: String, require: true },
+            Q4AnswerAContent: { type: String, required: true },
+            Q4AnswerBContent: { type: String, required: true },
+            Q4AnswerCContent: { type: String, required: true },
+            Q4AnswerDContent: { type: String, required: true },
+        },
+        Question5: {
+            Q5QuestionContent: { type: String, require: true },
+            Q5AnswerValue: { type: String, require: true },
+            Q5AnswerAContent: { type: String, required: true },
+            Q5AnswerBContent: { type: String, required: true },
+            Q5AnswerCContent: { type: String, required: true },
+            Q5AnswerDContent: { type: String, required: true },
+        }
+    }
+})
 
 var userInfo = mongoose.model("User", userSchema)
 var userScore = mongoose.model("userScore", userScoreSchema)
 var tracnghiem = mongoose.model("tracnghiem", tracnghiemSchema)
 var dientu = mongoose.model("dientu", dientuSchema)
+var baidoc = mongoose.model("baidoc", baidocSchema)
 
 module.exports = {
     userInfo: userInfo,
     userScore: userScore,
     tracnghiem: tracnghiem,
     dientu: dientu,
+    baidoc: baidoc
 }
