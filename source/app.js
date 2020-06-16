9e229325c5fea435c6476fe2c3c64caf11e7fa74
Cường Trần <46061394+cuonganh@users.noreply.github.com>
2020-06-17 01:21:24 +0700

    Update app.js

--- source/app.js
+++ source/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const path = require("path");
 const mongoose = require("mongoose");
+const session = require("express-session")
 const passport = require('passport');
+const cookieSession = require('cookie-session');
 
 
 let app = express();
@@ -9,17 +13,30 @@ let home = require("./routers/home")
 let createUser = require("./routers/createUser")
 let createTracNghiem = require("./routers/createTracNghiem")
 let createDienTu = require("./routers/createDienTu")
+let createBaiDoc = require("./routers/createBaiDoc")
+let test = require("./routers/test")
 let auth = require("./routers/auth")
 let userScore = require("./routers/userScore")
 let userInfo = require("./routers/userInfo")
 let allUser = require("./routers/allUser")
 let userInfoWithID = require("./routers/userInfoWithID")
+let allQuestions = require("./routers/allQuestions")
+let ERDiagram = require("./routers/ERDiagram")
+
 require("./routers/passport");
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: ["ratruongdunghan"]
+    })
+);
 app.use(passport.initialize());
 app.use(passport.session());
+
 
 
 app.engine("handlebars", handlebars({ defaultLayout: "main" }))
@@ -29,12 +46,15 @@ app.use("/", home)
 app.use("/", createUser)
 app.use("/", createTracNghiem)
 app.use("/", createDienTu)
+app.use("/", createBaiDoc)
 app.use("/", test)
 app.use("/", auth)
 app.use("/", userScore)
 app.use('/', userInfo)
 app.use("/", allUser)
 app.use("/", userInfoWithID)
+app.use("/allquestions", allQuestions)
+app.use("/", ERDiagram)
 
 app.use(express.static("public"));
 
